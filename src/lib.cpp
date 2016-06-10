01807e70477d54865aff96fc9671197b4d207a01
pr0gr4m <object_c@naver.com>
2016-06-10 23:02:03 +0900

    update

--- src/lib.cpp
+++ src/lib.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 #include <Windows.h>
 #include <conio.h>
@@ -39,46 +38,4 @@ void ClearReadBuff(void)
 {
 	std::cin.clear();
 	std::cin.ignore(std::cin.rdbuf()->in_avail(), '\n');
-=======
-#include <iostream>
-#include <Windows.h>
-#include <conio.h>
-
-int getKeyNoBuff(void)
-{
-	int ch = _getch();
-	if (ch == 224 || ch == 0)
-	{
-		ch = _getch();
-	}
-	return ch;
-}
-
-void gotoxy(int x, int y)
-{
-	COORD pos = { x, y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
-}
-
-void setConsoleSize(void)
-{
-	system("mode con: cols=200 lines=60");
-	system("cls");
-}
-
-void clrscr(void)
-{
-	system("cls");
-}
-
-void setColor(int colFrt, int colBck)
-{
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ((colBck << 4) | colFrt));
-}
-
-void ClearReadBuff(void)
-{
-	std::cin.clear();
-	std::cin.ignore(std::cin.rdbuf()->in_avail(), '\n');
->>>>>>> 17deda5118fcb09193b138e7a4a433c3e5a9407a
 }
